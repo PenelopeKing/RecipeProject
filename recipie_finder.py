f09116055174989e2e556e038cbe2e975cd00476
PenelopeKing <pby.king@gmail.com>
2022-04-11 19:15:28 -0700

    minor changes and progress to save

--- recipie_finder.py
+++ recipie_finder.py
@@ -7,13 +7,47 @@ Recipie Finder Project
 import pandas as pd
 import numpy as np
 
+class RecipeFinder:
 
-def process_data(filepath: str) -> pd.DataFrame():
-    """
-    
-    """
-    raw_df = pd.read_csv(filepath)
-    raw_df = raw_df.drop(columns = ['Unnamed: 0'])
-    return raw_df
+    df = None
+
+    def __init__(self, filepath : str):
+        """
+        
+        """
+        self.df = self.get_data(filepath)
+
+
+    def get_data(filepath: str) -> pd.DataFrame():
+        """
+        makes dataframe depending on what csv filepath input into function
+        input: String of the filepath to dataframes will be using
+        outout: panda DataFrame for that csv
+        """
+
+        raw_df = pd.read_csv(filepath)
+        raw_df = raw_df.drop(columns = ['Unnamed: 0'])
+        df= raw_df.get(['title','ingredients','link','NER'])
+        return df
+
+
+    def ing_match(ing : str):
+        """
+        
+        """
+
+
+    def find_recipe(ing_list : list) -> pd.DataFrame():
+        """
+        will return a DataFrame of reicpes that matches the input ingredients 
+        input: list of Strings (ingredients)
+        return: panda DataFrame (elements of data df that matches ing_list)
+        """
+
+
+# testing class RF
+
+filepath = 'datasets/recipe_nlg/RecipeNLG_dataset.csv'
+RF = RecipeFinder(filepath)
 
 
