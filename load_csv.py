fdbcb4ef671186ee6c57389b3f4399d389bc1d2d
PenelopeKing <pby.king@gmail.com>
2022-04-16 21:43:21 -0700

    loads in csv file from kaggle api

--- load_csv.py
+++ load_csv.py
@@ -1,21 +1,26 @@
 '''
 To load CSV file data
 Please run this file first before running other code in the file
+You should only need to run once ever
 '''
-'''
-import requests
-# kaggle datasets download -d paultimothymooney/recipenlg
-# https://www.kaggle.com/datasets/paultimothymooney/recipenlg?resource=download
-# response = requests.get("kaggle datasets download -d paultimothymooney/recipenlg")
-response = requests.get("https://www.kaggle.com/datasets/paultimothymooney/recipenlg?resource=download")
-print(response.status_code)
-
-if (response.status_code == 200):
-    data = response.json()
-'''
-
+import zipfile
 import pandas as pd
 
+# import in CSV with Kaggle API
+# https://www.kaggle.com/datasets/paultimothymooney/recipenlg?resource=download
+from kaggle.api.kaggle_api_extended import KaggleApi
+api = KaggleApi()
+api.authenticate()
+api.dataset_download_file('paultimothymooney/recipenlg', \
+    file_name = 'RecipeNLG_dataset.csv' ,  path='./datasets/')
+
+# unzips file
+with zipfile.ZipFile('datasets/RecipeNLG_dataset.csv.zip','r') as zipref:
+    zipref.extractall('./datasets/')
+
+# csv to panda dataframe
 filepath = 'datasets/RecipeNLG_dataset.csv'
 raw_df = pd.read_csv(filepath)
+
+# turns dataframe into a pickle file for easier access later on
 raw_df.to_pickle("pickled_data.pkl")
